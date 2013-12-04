12a360443af81e08ce88b03c87663e87efacaf2e
Monica Cardinal <9redcardinals@gmail.com>
2013-12-03 21:54:48 -0800

    Associated users and posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,6 +22,10 @@ class PostsController < ApplicationController
   end
 end
 
+  def create
+    @post = current_user.posts.build(params[:post])
+  end
+
   def edit
     @post = Post.find(params[:id])
   end
